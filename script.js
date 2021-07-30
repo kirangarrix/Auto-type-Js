0bcf1f84eb5cba9c30c0e2142412c6c8d811b137
kiran garrix <kirangarrix2019@gmail.com>
2021-07-30 12:41:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const texts=['Website','Games','System Sofwares'];
+let count=0;
+let index=0;
+
+let currentText='';
+let letters='';
+
+(function type(){
+    if(count===texts.length){
+        count=0;
+    }
+    currentText=texts[count];
+    letters=currentText.slice(0,++index);
+
+    document.querySelector('.typing').textContent=letters;
+    if(letters.length===currentText.length){
+          count++;
+          index=0;
+    }
+    setTimeout(type,300);
+
+}());
